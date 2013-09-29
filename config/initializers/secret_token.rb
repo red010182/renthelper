d6dd89835f3fbbff537b16eb413bebf92e0693b7
Alston <p.truesword@gmail.com>
2013-09-29 19:08:48 +0800

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Renthelper::Application.config.secret_key_base = 'b2065901ece0e6fd2b5fcbeb8d0fa13fea57603e87427779e8835f28e2f4f705f812f4fe3cfc6c967a4c2a454d506624c6eb4379bb15dbc0c2ab86596a56c173'
+Renthelper::Application.config.secret_key_base = 'da96735f247859eb5da48e327bccaa7bcd1f3b3bc22d72bf3fea91b0c2750953ea766d053510f4d028863d9790af44b9c80b9dcfd07048131c82e2115a2f90a3'
